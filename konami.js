bb1e91dd0e59b06632be52c92d56ba47a896fda0
Kevin <pykevin@gmail.com>
2017-06-26 22:04:42 +0200

    Initial commit

---
+++ konami.js
@@ -0,0 +1,14 @@
+function konami() {
+
+	const arr = [];
+	const konami = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
+
+	document.addEventListener('keyup', function(e) {
+		arr.push(e.keyCode);
+		if (arr.length >= 12) arr.shift();
+		if (arr.toString() === konami.toString()) cheatMode();
+	});
+
+}
+
+function cheatMode() {}
